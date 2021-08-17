1b1e2b9444ac1552a210a959573df9dc6b153cfc
yaucheny <2879105@gmail.com>
2021-08-17 13:49:33 +0300

    liqubase complit

--- core/src/main/java/com/exposit/domain/model/entity/BaseEntity.java
+++ core/src/main/java/com/exposit/domain/model/entity/BaseEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     protected Long id;
 }
